75bfef1472ac8b705a7aaa295a86d139fe711ea6
YakinaMu <65917086+YakinaMu@users.noreply.github.com>
2020-05-30 17:32:33 +0300

    Update Source5.cpp

--- lab3/Source5.cpp
+++ lab3/Source5.cpp
@@ -4,85 +4,78 @@
 #include <fstream>
 #include<Windows.h>
 using namespace std;
+
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	setlocale(0, "ru");
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+setlocale(0, "ru");
 
-	string path = "F.txt";
-	ifstream fin;
-	fin.open(path);
-	if (!fin.is_open())
-	{
-		cout << "error";
-	}
-	else
-	{
-		char ch;
-		vector<char>v;
-		while (fin.get(ch))
-			v.push_back(ch);
-
-		int n = v.size();
-
-		char* mass1 = new char[n];    //строка
-		const char mass2[7] = { ' ',',','.','!','?',':',';' }; //исключаемые символы
-
-		for (int i = 0; i < v.size(); i++)
-		{
-			mass1[i] = v[i];
-
-		}
-		for (int i = 0; i < n; i++)
-		{
-			cout << mass1[i] << " ";
-		}
-		cout << endl;
-
-
-		cout << "Введите сдвиг " << endl;
-		int k;        //сдвиг
-		int count = 0;
-
-		cin >> k;
-		if (k < 1 || k>10) { cout << "error"; }
-		else
-		{
-
-			for (int j = 0; j < n; j++)
-			{
-				count = 0;
-				for (int i = 0; i < 7; i++)
-				{
-					if (mass1[j] != mass2[i])
-					{
-						count++;
-					}
-				}
-				if (count == 7)
-				{
-					mass1[j] = mass1[j] + k;
-				}
-				cout << mass1[j];
-			}
-		}
-		ofstream fout;
-		fout.open(path);
-		if (!fout.is_open())
-		{
-			cout << "error";
-		}
-		else
-		{
-			for (int i = 0; i < n; i++)
-			{
-				fout << mass1[i];
-			}
-			fout.close();
-		}
-		fin.close();
-	}
+string path = "F3.txt";
+ifstream fin;
+fin.open(path);
+if (!fin.is_open())
+{
+cout << "error";
 }
+else
+{
+char ch;
+vector<char>v;
+while (fin.get(ch))
+v.push_back(ch);
 
+int n = v.size();
+char* mass1 = new char[n]; //СЃС‚СЂРѕРєР°
+const char mass2[7] = { ' ',',','.','!','?',':',';' }; //РёСЃРєР»СЋС‡Р°РµРјС‹Рµ СЃРёРјРІРѕР»С‹
+for (int i = 0; i < v.size(); i++)
+{
+mass1[i] = v[i];
+}
+for (int i = 0; i < n; i++)
+{
+cout << mass1[i];
+}
+cout << endl;
+cout << "Р’РІРµРґРёС‚Рµ СЃРґРІРёРі " << endl;
 
+int k; //СЃРґРІРёРі
+int count = 0;
+cin >> k;
+if (k < 1 || k>10) { cout << "error"; }
+else
+{
+for (int j = 0; j < n; j++)
+{
+count = 0;
+for (int i = 0; i < 7; i++)
+{
+if (mass1[j] != mass2[i])
+{
+count++;
+}
+}
+if (count == 7)
+{
+mass1[j] = mass1[j] + k;
+}
+cout << mass1[j];
+}
+}
+ofstream fout;
+fout.open(path);
+if (!fout.is_open())
+{
+cout << "error";
+}
+else
+{
+for (int i = 0; i < n; i++)
+{
+fout << mass1[i];
+}
+fout.close();
+}
+fin.close();
+}
+}
