cc80e148acba07d48b3f641a1d605555632a804e
YakinaMu <65917086+YakinaMu@users.noreply.github.com>
2020-05-30 17:30:54 +0300

    Update Source4.cpp

--- lab3/Source4.cpp
+++ lab3/Source4.cpp
@@ -4,50 +4,51 @@
 #include<Windows.h>
 #include<vector>
 using namespace std;
+
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	setlocale(0, "ru");
-	ifstream fin;
-	fin.open("F.txt");
-	if (!fin.is_open()) cout << "Error\n";
-	else
-	{
-		cout << "Äàííûå âçÿòûå èç ôàéëà: " << endl;
-		string str;
-		while (!fin.eof())
-		{
-			str = "";
-			getline(fin, str);
-			cout << str << endl;
-		}
-		vector<string> myVector;
-		string temp;
-		for (int i = 0; i < str.length(); i++) {
-			if (str[i] == ' ') {
-				myVector.push_back(temp);
-				temp.clear();
-			}
-			else if (i + 1 == str.length()) {
-				temp += str[i];
-				myVector.push_back(temp);
-			}
-			else
-				temp += str[i];
-		}
-		ofstream fout;
-		fout.open("F1.txt");
-		for (int i = 0; i < myVector.size(); i++)
-		{
-			string s = myVector[i];
-			if (s[0] == 'ñ' || s[0] == 'Ñ') {
-				fout << myVector[i] << endl;
-			}
-		}
-		fout.close();
-	}
-	fin.close();
-	return 0;
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+setlocale(0, "ru");
+	
+ifstream fin;
+fin.open("F2.txt");
+if (!fin.is_open()) cout << "Error\n";
+else
+{
+cout << "Ð”Ð°Ð½Ð½Ñ‹Ðµ Ð²Ð·ÑÑ‚Ñ‹Ðµ Ð¸Ð· Ñ„Ð°Ð¹Ð»Ð°: " << endl;
+string str;
+while (!fin.eof())
+{
+str = "";
+getline(fin, str);
+cout << str << endl;
 }
-
+vector<string> myVector;
+string temp;
+for (int i = 0; i < str.length(); i++) {
+if (str[i] == ' ') {
+myVector.push_back(temp);
+temp.clear();
+}
+else if (i + 1 == str.length()) {
+temp += str[i];
+myVector.push_back(temp);
+}
+else
+temp += str[i];
+}
+ofstream fout;
+fout.open("newF.txt");
+for (int i = 0; i < myVector.size(); i++)
+{
+string s = myVector[i];
+if (s[0] == 'Ñ' || s[0] == 'Ð¡') {
+fout << myVector[i] << endl;
+}
+}
+fout.close();
+}
+fin.close();
+return 0;
+}
